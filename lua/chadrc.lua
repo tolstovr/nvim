2ec64291006baabfc611446f9c88316fe80af32b
tolstovr <tolstovrob@gmail.com>
2024-10-06 14:39:40 +0400

    feat:  add basic ui config

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,17 +1,34 @@
--- This file needs to have same structure as nvconfig.lua 
--- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- This file  needs to have same structure as nvconfig.lua
+-- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
-M.base46 = {
-	theme = "onedark",
+M.ui = {
+  theme = "github_dark",
+  statusline = {
+    theme = "vscode_colored",
+  },
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+    DiffChange = {
+      bg = "#464414",
+      fg = "none",
+    },
+    DiffAdd = {
+      bg = "#103507",
+      fg = "none",
+    },
+    DiffRemoved = {
+      bg = "#461414",
+      fg = "none",
+    },
+  },
+}
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+M.base46 = {
+  transparency = true,
 }
 
 return M
