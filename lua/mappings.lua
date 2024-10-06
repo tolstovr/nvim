3aa0c7f5fa22f89c276b9c0289590d5d2fda0d03
tolstovr <tolstovrob@gmail.com>
2024-10-06 16:17:51 +0400

    feat(keymaps):  add basic keymaps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,64 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
+map("n", "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Find Todo" })
+map("n", "\\", "<cmd>:vsplit <CR>", { desc = "Vertical Split" })
+
+-- Trouble
+map("n", "<leader>qx", "<cmd>TroubleToggle<CR>", { desc = "Open Trouble" })
+map("n", "<leader>qw", "<cmd>TroubleToggle workspace_diagnostics<CR>", { desc = "Open Workspace Trouble" })
+map("n", "<leader>qd", "<cmd>TroubleToggle document_diagnostics<CR>", { desc = "Open Document Trouble" })
+map("n", "<leader>qq", "<cmd>TroubleToggle quickfix<CR>", { desc = "Open Quickfix" })
+map("n", "<leader>ql", "<cmd>TroubleToggle loclist<CR>", { desc = "Open Location List" })
+map("n", "<leader>qt", "<cmd>TodoTrouble<CR>", { desc = "Open Todo Trouble" })
+
+-- Debug
+map("n", "<leader>du", function()
+  require("dapui").toggle()
+end, { desc = "Toggle Debug UI" })
+map("n", "<leader>db", function()
+  require("dap").toggle_breakpoint()
+end, { desc = "Toggle Breakpoint" })
+map("n", "<leader>ds", function()
+  require("dap").continue()
+end, { desc = "Start" })
+map("n", "<leader>dn", function()
+  require("dap").step_over()
+end, { desc = "Step Over" })
+
+-- Git
+map("n", "<leader>gl", ":Flog<CR>", { desc = "Git Log" })
+map("n", "<leader>gf", ":DiffviewFileHistory<CR>", { desc = "Git File History" })
+map("n", "<leader>gc", ":DiffviewOpen HEAD~1<CR>", { desc = "Git Last Commit" })
+map("n", "<leader>gt", ":DiffviewToggleFile<CR>", { desc = "Git File History" })
+
+-- Terminal
+map("n", "<C-]>", function()
+  require("nvchad.term").toggle { pos = "vsp", size = 0.4 }
+end, { desc = "Toogle Terminal Vertical" })
+map("n", "<C-\\>", function()
+  require("nvchad.term").toggle { pos = "sp", size = 0.4 }
+end, { desc = "Toogle Terminal Horizontal" })
+map("n", "<C-f>", function()
+  require("nvchad.term").toggle { pos = "float" }
+end, { desc = "Toogle Terminal Float" })
+map("t", "<C-]>", function()
+  require("nvchad.term").toggle { pos = "vsp" }
+end, { desc = "Toogle Terminal Vertical" })
+map("t", "<C-\\>", function()
+  require("nvchad.term").toggle { pos = "sp" }
+end, { desc = "Toogle Terminal Horizontal" })
+map("t", "<C-f>", function()
+  require("nvchad.term").toggle { pos = "float" }
+end, { desc = "Toogle Terminal Float" })
+
+-- Basic
+
 map("i", "jk", "<ESC>")
+map("i", "<C-g>", function()
+  return vim.fn["codeium#Accept"]()
+end, { expr = true })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
