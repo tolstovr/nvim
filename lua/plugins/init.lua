be03e454f8a2c5e558f490391342b5b9b5fafbf9
tolstovr <tolstovrob@gmail.com>
2024-10-09 11:12:11 +0400

    feat(pligins):  add emmet abbreviations

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -137,4 +137,10 @@ return {
       })
     end,
   },
+  {
+    "olrtg/nvim-emmet",
+    config = function()
+      vim.keymap.set({ "n", "v" }, '<leader>xe', require('nvim-emmet').wrap_with_abbreviation)
+    end,
+  },
 }
